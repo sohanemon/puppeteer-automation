0037cb4dc2bf5532c2778b044f2f40824121e4e1
sohanemon <m.sohanemon@gmail.com>
2023-05-15 20:02:56 +0600

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { NavigationMenu } from '@/components/ui/navigation-menu';
+
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className=''>
+      <NavigationMenu />
+    </main>
+  );
 }
