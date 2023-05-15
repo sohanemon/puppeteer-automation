1c18e101d5ed077e38afbe68f66d3d7d5d22400d
sohanemon <m.sohanemon@gmail.com>
2023-05-15 12:04:34 +0000

    May 15, 2023, 6:04 PM

--- src/app/puppeteer/route.ts
+++ src/app/puppeteer/route.ts
@@ -5,15 +5,13 @@ export async function GET(req: Request) {
   const url = 'https://books.toscrape.com';
   const b = new Browser();
   const page = await b.newPage();
-  page.goto(url);
+  await page.goto(url);
 
-  await (
-    await page
-  ).evaluate(() => {
+  const book = await page.evaluate(() => {
     const classes = document.getElementsByClassName('image_container');
     console.log(classes);
   });
 
-  b.close();
+  await b.close();
   return NextResponse.json('Welcome in web scraping');
 }
