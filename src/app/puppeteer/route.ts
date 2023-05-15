4b1c4b035cd788373013810f07a401e64759c0cc
sohanemon <m.sohanemon@gmail.com>
2023-05-15 12:19:27 +0000

    May 15, 2023, 6:19 PM

--- src/app/puppeteer/route.ts
+++ src/app/puppeteer/route.ts
@@ -7,15 +7,14 @@ export async function GET(req: Request) {
   const page = await b.newPage();
   await page.goto(url);
 
-  const book = await page.evaluate(() => {
-    const list = Array.from(
+  const book = await page.evaluate(() =>
+    Array.from(
       document.querySelectorAll(
-        '#default > div.container-fluid.page > div > div > div > section > div:nth-child(2) > ol > li'
+        '#default > div.container-fluid.page > div > div > div > section > div > ol > li'
       )
-    );
-    console.log(list);
-  });
-
+    )
+  );
+  book.map((el) => console.log(el));
   await b.close();
 
   return NextResponse.json('lol');
