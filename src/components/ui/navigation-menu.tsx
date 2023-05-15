1b924abd9f774fc1fd2258bbf4664e58d7d5d770
sohanemon <m.sohanemon@gmail.com>
2023-05-15 14:44:35 +0000

    May 15, 2023, 8:44 PM

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -7,14 +7,13 @@ import { CaretDownIcon } from '@radix-ui/react-icons';
 export default function NavigationMenuDemo() {
   return (
     <>
-      {' '}
-      <NavigationMenu.Root className='relative z-[1] flex w-screen justify-center text-black'>
+      <NavigationMenu.Root className='relative z-[1] flex w-screen justify-center text-neutral-200'>
         <NavigationMenu.List className='center shadow-blackA7 m-0 flex list-none rounded-[6px] bg-neutral-700 p-1 shadow-[0_2px_10px]'>
           <NavigationMenu.Item>
-            <NavigationMenu.Trigger className='text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
+            <NavigationMenu.Trigger className='hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
               Learn{' '}
               <CaretDownIcon
-                className='text-violet10 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
+                className='relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
                 aria-hidden
               />
             </NavigationMenu.Trigger>
@@ -41,7 +40,7 @@ export default function NavigationMenuDemo() {
                       <div className='mt-4 mb-[7px] text-[18px] font-medium leading-[1.2] text-white'>
                         Radix Primitives
                       </div>
-                      <p className='text-mauve4 text-[14px] leading-[1.3]'>
+                      <p className=' text-[14px] leading-[1.3]'>
                         Unstyled, accessible components for React.
                       </p>
                     </a>
@@ -62,10 +61,10 @@ export default function NavigationMenuDemo() {
           </NavigationMenu.Item>
 
           <NavigationMenu.Item>
-            <NavigationMenu.Trigger className='text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
+            <NavigationMenu.Trigger className=' hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
               Overview{' '}
               <CaretDownIcon
-                className='text-violet10 relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
+                className=' relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
                 aria-hidden
               />
             </NavigationMenu.Trigger>
@@ -114,7 +113,7 @@ export default function NavigationMenuDemo() {
 
           <NavigationMenu.Item>
             <NavigationMenu.Link
-              className='text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]'
+              className=' hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]'
               href='https://github.com/radix-ui'
             >
               Github
@@ -152,10 +151,8 @@ const ListItem = forwardRef<any, Props>(
           ref={forwardedRef}
           href={href}
         >
-          <div className='text-violet12 mb-[5px] font-medium leading-[1.2]'>
-            {title}
-          </div>
-          <p className='text-mauve11 leading-[1.4]'>{children}</p>
+          <div className='mb-[5px] font-medium leading-[1.2]'>{title}</div>
+          <p className='leading-[1.4]'>{children}</p>
         </a>
       </NavigationMenu.Link>
     </li>
