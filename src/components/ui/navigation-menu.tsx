efc464ee67f2eedcbfa4f67fda0ead6dc53b7121
sohanemon <m.sohanemon@gmail.com>
2023-05-15 14:42:53 +0000

    May 15, 2023, 8:42 PM

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -8,8 +8,8 @@ export default function NavigationMenuDemo() {
   return (
     <>
       {' '}
-      <NavigationMenu.Root className='relative z-[1] flex w-screen justify-center'>
-        <NavigationMenu.List className='center shadow-blackA7 m-0 flex list-none rounded-[6px] bg-white p-1 shadow-[0_2px_10px]'>
+      <NavigationMenu.Root className='relative z-[1] flex w-screen justify-center text-black'>
+        <NavigationMenu.List className='center shadow-blackA7 m-0 flex list-none rounded-[6px] bg-neutral-700 p-1 shadow-[0_2px_10px]'>
           <NavigationMenu.Item>
             <NavigationMenu.Trigger className='text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
               Learn{' '}
