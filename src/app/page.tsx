51b25274b1dced012dbe6e357c228445148f5d65
sohanemon <m.sohanemon@gmail.com>
2023-05-15 14:41:35 +0000

    May 15, 2023, 8:41 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import NavigationMenuDemo from '@/components/ui/navigation-menu';
+
 export default function Home() {
   return (
-    <main className='text-7xl'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ex itaque
-      natus odit ullam facere, est quisquam odio et excepturi nostrum totam,
-      repellat, cum dicta voluptate fugiat quidem voluptatem perferendis?
+    <main className=''>
+      <NavigationMenuDemo />
     </main>
   );
 }
