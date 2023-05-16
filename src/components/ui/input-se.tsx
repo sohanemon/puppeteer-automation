01e8915143e1e0573d652aba3369d1c0a8c9bae0
sohanemon <m.sohanemon@gmail.com>
2023-05-16 02:30:05 +0000

    May 16, 2023, 8:30 AM

--- src/components/ui/input-se.tsx
+++ src/components/ui/input-se.tsx
@@ -4,20 +4,22 @@ import * as Label from '@radix-ui/react-label';
 export default function InputSe() {
   return (
     <>
-      <div className='flex items-center gap-[15px] px-5 @container w-1/2 mx-auto fixed inset-0'>
-        <Label.Root
-          className='text-[15px] font-medium leading-[35px] whitespace-nowrap text-white'
-          htmlFor='toScrape'
-        >
-          Website URL
-        </Label.Root>
-        <input
-          className='inline-flex w-full px-4 py-2 rounded-md bg-neutral-700'
-          type='text'
-          id='toScrape'
-          defaultValue='https://github.com/sohanemon'
-        />
-        <button className='px-4 py-2 tracking-wide rounded-md bg-neutral-800'>
+      <div className='space-y-2 px-5 @container w-1/2 mx-auto'>
+        <div>
+          <Label.Root
+            className='text-[15px] font-medium leading-[35px] whitespace-nowrap text-white'
+            htmlFor='toScrape'
+          >
+            Website URL
+          </Label.Root>
+          <input
+            className='inline-flex w-full px-4 py-2 rounded-md bg-neutral-700'
+            type='text'
+            id='toScrape'
+            defaultValue='https://github.com/sohanemon'
+          />
+        </div>
+        <button className='px-4 py-2 ml-auto tracking-wide rounded-md bg-neutral-800 '>
           Scrap
         </button>
       </div>
