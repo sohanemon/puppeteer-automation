1780d4e341218e1f19a409386e3515c47d5eb141
sohanemon <m.sohanemon@gmail.com>
2023-05-16 02:26:02 +0000

    May 16, 2023, 8:26 AM

--- src/components/ui/input-se.tsx
+++ src/components/ui/input-se.tsx
@@ -4,19 +4,22 @@ import * as Label from '@radix-ui/react-label';
 export default function InputSe() {
   return (
     <>
-      <div className='flex flex-wrap items-center gap-[15px] px-5 @container w-full mx-auto'>
+      <div className='flex items-center gap-[15px] px-5 @container w-96 mx-auto fixed inset-0'>
         <Label.Root
-          className='text-[15px] font-medium leading-[35px] text-white'
-          htmlFor='firstName'
+          className='text-[15px] font-medium leading-[35px] whitespace-nowrap text-white'
+          htmlFor='toScrape'
         >
-          First name
+          Website URL
         </Label.Root>
         <input
-          className=' inline-flex h-[35px] w-[200px] bg-white'
+          className='inline-flex w-full px-4 py-2 rounded-md bg-neutral-700'
           type='text'
-          id='firstName'
-          defaultValue='Pedro Duarte'
+          id='toScrape'
+          defaultValue='https://github.com/sohanemon'
         />
+        <button className='px-4 py-2 tracking-wide rounded-md bg-neutral-800'>
+          Scrap
+        </button>
       </div>
     </>
   );
