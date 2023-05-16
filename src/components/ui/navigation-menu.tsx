b03506fb20def9164566881f2a68eb48162e1dee
sohanemon <m.sohanemon@gmail.com>
2023-05-16 01:28:37 +0000

    May 16, 2023, 7:28 AM

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -17,13 +17,13 @@ export default function NavigationMenuDemo() {
                 aria-hidden
               />
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className='data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto'>
+            <NavigationMenu.Content className='bg-neutral-700 data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto'>
               <ul className='one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]'>
                 <li className='grid row-span-3'>
                   <NavigationMenu.Link asChild>
                     <a
-                      className='focus:shadow-violet7 from-purple9 to-indigo9 flex
-                    h-full w-full select-none flex-col justify-end rounded-[6px] bg-gradient-to-b p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px]'
+                      className='flex
+                    h-full w-full select-none flex-col justify-end rounded-[6px] bg-neutral-700 p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px]'
                       href='/'
                     >
                       <svg
@@ -55,57 +55,6 @@ export default function NavigationMenuDemo() {
                 </ListItem>
                 <ListItem href='https://icons.radix-ui.com/' title='Icons'>
                   A crisp set of 15x15 icons, balanced and consistent.
-                </ListItem>
-              </ul>
-            </NavigationMenu.Content>
-          </NavigationMenu.Item>
-
-          <NavigationMenu.Item>
-            <NavigationMenu.Trigger className=' hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'>
-              Overview{' '}
-              <CaretDownIcon
-                className=' relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
-                aria-hidden
-              />
-            </NavigationMenu.Trigger>
-            <NavigationMenu.Content className='absolute top-0 left-0 w-full sm:w-auto'>
-              <ul className='m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3'>
-                <ListItem
-                  title='Introduction'
-                  href='/docs/primitives/overview/introduction'
-                >
-                  Build high-quality, accessible design systems and web apps.
-                </ListItem>
-                <ListItem
-                  title='Getting started'
-                  href='/docs/primitives/overview/getting-started'
-                >
-                  A quick tutorial to get you up and running with Radix
-                  Primitives.
-                </ListItem>
-                <ListItem
-                  title='Styling'
-                  href='/docs/primitives/overview/styling'
-                >
-                  Unstyled and compatible with any styling solution.
-                </ListItem>
-                <ListItem
-                  title='Animation'
-                  href='/docs/primitives/overview/animation'
-                >
-                  Use CSS keyframes or any animation library of your choice.
-                </ListItem>
-                <ListItem
-                  title='Accessibility'
-                  href='/docs/primitives/overview/accessibility'
-                >
-                  Tested in a range of browsers and assistive technologies.
-                </ListItem>
-                <ListItem
-                  title='Releases'
-                  href='/docs/primitives/overview/releases'
-                >
-                  Radix Primitives releases and their changelogs.
                 </ListItem>
               </ul>
             </NavigationMenu.Content>
